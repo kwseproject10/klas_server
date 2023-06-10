6cf1ecba823ca47166f3ceb0495642815559221d
TAEY00N <3daysplit@naver.com>
2023-06-10 23:12:35 +0900

    userInform.js 완

--- routes/userInform.js
+++ routes/userInform.js
@@ -3,14 +3,14 @@ const router = express.Router();
 const connection = require("../modules/mysql");
 
 router.get("/", (req, res) => {
-    // 쿼리 파라미터 추출
-    const userID = parseInt(req.query.userID);
-  
-    // Check if userID is NaN and set it to null
-    if (isNaN(userID)) {
-      userID = null;
-    }
-    /* MySQL 쿼리 유저 정보
+  // 쿼리 파라미터 추출
+  const userID = parseInt(req.query.userID);
+
+  // Check if userID is NaN and set it to null
+  if (isNaN(userID)) {
+    userID = null;
+  }
+  /* MySQL 쿼리 유저 정보
       name: "홍길동",
       type: "학부생",
       major: "컴퓨터정보공학부",
@@ -24,35 +24,35 @@ router.get("/", (req, res) => {
       advisorEmail: "kihoonlee@kw.ac.kr",
       advisorNum: "02-940-8674",
       state: "재학")*/
-    const query =
-      "";
-  
-    connection.query(query, [userID], (err, results) => {
-      if (err) {
-        console.error("MySQL query error: ", err);
-        res.status(500).json({ error: "Internal server error" });
-        return;
-      }
-      if (results.length > 0) {
-        const response = {
-          name: result[0].name,
-          type: result[0].type,
-          major: result[0].major,
-          ID: result[0].ID,
-          grade: result[0].grade,
-          numberOfTerm: result[0].numberOfTerm,
-          email: result[0].email,
-          phoneNum: result[0].phoneNum,
-          birthday: result[0].birthday,
-          advisor: result[0].advisor,
-          advisorEmail: result[0].advisorEmail,
-          advisorNum: result[0].advisorNum,
-          state: result[0].state
-        };
-        res.json(response);
-      } 
-    });
+  const query =
+    "select u.userName, u.userType, m.majorName as major, u.userID as ID, s.grade, s.semesterNum as numberOfTerm, u.email, u.phone as phoneNum, u.birth as birthday, (SELECT u.userName where u.userID = s.advisor) as advisor, (SELECT u.email where u.userID = s.advisor) as advisorEmail, (SELECT u.tel where u.userID = s.advisor) as advisorNum, s.state FROM users AS u left JOIN majors AS m ON u.majorID = m.majorID left JOIN students AS s ON u.userID = s.advisor where u.userID = ?;";
+
+  connection.query(query, [userID], (err, results) => {
+    if (err) {
+      console.error("MySQL query error: ", err);
+      res.status(500).json({ error: "Internal server error" });
+      return;
+    }
+    if (results.length > 0) {
+      const result = results[0];
+      const response = {
+        name: result.userName || "",
+        type: result.userType || "",
+        major: result.major || "",
+        ID: result.ID || "",
+        grade: result.grade || "",
+        numberOfTerm: result.numberOfTerm || "",
+        email: result.email || "",
+        phoneNum: result.phoneNum || "",
+        birthday: result.birthday || "",
+        advisor: result.advisor || "",
+        advisorEmail: result.advisorEmail || "",
+        advisorNum: result.advisorNum || "",
+        state: result.state || "",
+      };
+      res.json(response);
+    }
   });
-  
+});
 
 module.exports = router;
