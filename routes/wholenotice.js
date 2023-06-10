c7914443a57c36d6f7116da53269a7c003d88220
TAEY00N <3daysplit@naver.com>
2023-06-11 06:02:52 +0900

    wholenotice.js 베타

--- routes/wholenotice.js
+++ routes/wholenotice.js
@@ -12,38 +12,6 @@ router.get("/", (req, res) => {
     userID = null;
   }
 
-  /* [
-      {유저 ID 보내면 이번 학기 수강 중인 lecture들의 notice list 반환
-        key: "0",
-        title: "중간고사 시험범위",
-        subject: "소프트웨어공학",
-        date: "2022.01.01(월)"
-      },
-      {
-        key: "1",
-        title: "2차 프로젝트 점수 공지",
-        subject: "소프트웨어공학",
-        date: "2023.05.28(일)"
-      },
-      {
-        key: "2",
-        title: "[자료] uCoS 예제소스 컴파일 시 발생하는 문제 해결방법",
-        subject: "임베디드시스템S/W설계",
-        date: "2022.01.01(월)"
-      },
-      {
-        key: "3",
-        title: "Assignment for INCOMPLETE PREREQUISITE",
-        subject: "신호및시스템",
-        date: "2022.01.01(월)"
-      },
-      {
-        key: "4",
-        title: "금일 수업 휴강 공지",
-        subject: "머신러닝",
-        date: "2022.01.01(월)"
-      }
-    ]*/
   const query =
     "select b.title, s.subjectName as lecName, b.boardDate as noticeDate from enrollments as e left join boards as b on e.lecKey = b.lecKey join lectures as l on e.leckey = l.leckey join subjects as s on l.subjectID = s.subjectID where b.boardType='notice' and e.studentID = ?;";
 
