02ab3b41d55c42200daa6f7872214bf98a0f1d35
Highdown <37868047+Highdown@users.noreply.github.com>
2023-06-15 18:01:09 +0900

    Update enrolllecture.js

--- routes/enrolllecture.js
+++ routes/enrolllecture.js
@@ -3,26 +3,26 @@ const router = express.Router();
 const connection = require("../modules/mysql");
 
 // /enrolllecture?userID=*&lectureID=*
-router.get("/", (req, res) => {
+router.post("/", (req, res) => {
   // 쿼리 파라미터 추출
-  const userID = parseInt(req.query.userID);
-  const lectureID = req.query.lectureID;
+  //const userID = parseInt(req.body.userID);
+  //const lectureID = req.body.lectureID;
 
   // Check if userID is NaN and set it to null
-  if (isNaN(userID)) {
+  const data = req.body;
+  /*if (isNaN(data.userID)) {
     userID = null;
   }
-
   // Check if password is NULL and set it to null
   if (lectureID === "NULL") {
     lectureID = null;
-  }
+  }*/  
 
   // 유저 ID와 강의 ID 보내면 해당 유저 수강 강의 추가(수강 신청) -> true를 정의하려면 뭔갈 보내줘야함
   const query =
     "";
 
-  connection.query(query, [userID, lectureID], (err, results) => {
+  connection.query(query, data, (err, results) => {
     if (err) {
       console.error("MySQL query error: ", err);
       res.status(500).json({ error: "Internal server error" });
