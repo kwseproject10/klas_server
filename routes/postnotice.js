e3fa44509354c4445e2792425b858a4094c1c21c
TAEY00N <3daysplit@naver.com>
2023-06-16 06:25:25 +0900

    postnotice 수정중

--- routes/postnotice.js
+++ routes/postnotice.js
@@ -4,66 +4,64 @@ const connection = require("../modules/mysql");
 
 /*
 {
-    "lectureID": lectureID,
-    "notice": notice,
-    "title": title,
-    "cont": cont,
-    "date": date,
-    "poster": poster,
+	"lectureID": "1111-1-1111-1",
+	title:"제목",
+	content:"본문 내용",
+	poster:"사람이름",
+	fileURL:"~~~~~~",
 }
 */
 
 // /postnotice
 router.post("/", (req, res) => {
-    const data = req.body;
+  const data = req.body;
+  const lectureID = data.lectureID;
 
-    const query1 = "";
+  const query1 =
+    "SELECT lecKey FROM lectures l where concat(l.majID,'-',l.lecLv,'-',l.subID,'-',l.clsNum) = ?";
 
-    connection.query(query1, [data.lectureID], (error, result1) => {
-        if (error) {
-            // 아예 db 접근이 안되는 경우
-            console.error("MySQL query error: ", error);
-            res.status(500).json({ error: "Internal server error" });
+  connection.query(query1, [lectureID], (error, result1) => {
+    if (error) {
+      // 아예 db 접근이 안되는 경우
+      console.error("MySQL query error: ", error);
 
-            return;
-        } else {
-            if (result1.length > 0) {
+      return res.status(500).json({ error: "Internal server error" });
+    } else {
+      if (result1.length > 0) {
+        hit = "0";
+        type = "notice";
+        const query3 = "";
 
-                hit = "0";
-                type = "notice";
-                const query3 =
-                    "";
+        connection.query(
+          query3,
+          [
+            data.lectureID,
+            notice,
+            data.title,
+            data.cont || null,
+            hit,
+            data.date,
+            data.poster,
+          ],
+          (error, result3) => {
+            if (error) {
+              console.error("MySQL 저장 실패:", error);
 
-                connection.query(
-                    query3,
-                    [
-                        data.lectureID,
-                        notice,
-                        data.title,
-                        data.cont || null,
-                        hit,
-                        data.date,
-                        data.poster,
-                    ],
-                    (error, result3) => {
-                        if (error) {
-                            console.error("MySQL 저장 실패:", error);
-
-                            res.json({ result: false });
-                        } else {
-                            console.log("MySQL 저장 성공:", result3);
-
-                            res.json({ result: true });
-                        }
-                    }
-                );
+              res.json({ result: false });
             } else {
-                console.log("존재하지 않는 강의 ID입니다.");
+              console.log("MySQL 저장 성공:", result3);
 
-                res.json({ result: false });
+              res.json({ result: true });
             }
-        }
-    });
+          }
+        );
+      } else {
+        console.log("존재하지 않는 강의 ID입니다.");
+
+        res.json({ result: false });
+      }
+    }
+  });
 });
 
 module.exports = router;
