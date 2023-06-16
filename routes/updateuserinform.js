3ba0936fa6d862b8850812269b9cb02bc8f31c1e
TAEY00N <3daysplit@naver.com>
2023-06-16 17:54:55 +0900

    이미지 삭제 후 저장으로 변경

--- routes/updateuserinform.js
+++ routes/updateuserinform.js
@@ -53,6 +53,83 @@ router.post("/", upload.single("file"), (req, res) => {
 
   const queries = [];
 
+  // 기존 이미지 정보 조회
+  const querySelect = "SELECT * FROM userfiles WHERE userID = ?";
+  const valueSelect = [userID];
+  queries.push(executeQuery(querySelect, valueSelect));
+
+  if (file) {
+    const queryInsert =
+      "INSERT INTO userfiles (userID, ufName, ufPath, ufSize, ufRName) VALUES (?, ?, ?, ?, ?)";
+    const valueInsert = [
+      userID,
+      fileName,
+      filePath,
+      fileSize,
+      originalFileName,
+    ];
+    queries.push(executeQuery(queryInsert, valueInsert));
+  }
+
+  const queryUpdate =
+    "UPDATE users SET email = ?, phone = ?, pw = ? WHERE userID = ?";
+  const valueUpdate = [email, phone, pw, userID];
+  queries.push(executeQuery(queryUpdate, valueUpdate));
+
+  Promise.all(queries)
+    .then((results) => {
+      console.log("Results:", results);
+
+      // 기존 이미지 삭제
+      const existingImage = results[0][0];
+      if (existingImage) {
+        const queryDelete = "DELETE FROM userfiles WHERE userID = ?";
+        const valueDelete = [userID];
+        return executeQuery(queryDelete, valueDelete);
+      }
+    })
+    .then(() => {
+      res.status(200).json({
+        result: true,
+      });
+    })
+    .catch((error) => {
+      console.error("Error saving file to database:", error);
+      res.status(500).send("Failed to save file to database");
+    });
+});
+
+/*
+
+
+
+
+
+router.post("/", upload.single("file"), (req, res) => {
+  const file = req.file;
+  console.log(file);
+
+  // 파일 정보
+  let fileName = null;
+  let originalFileName = null;
+  let filePath = null;
+  let fileSize = null;
+
+  if (file) {
+    fileName = file.filename;
+    originalFileName = file.originalname;
+    filePath = file.path;
+    fileSize = file.size;
+  }
+
+  // 다른 필드에서 전송된 정보
+  const userID = req.body.userID;
+  const email = req.body.Email;
+  const phone = req.body.PhoneNum;
+  const pw = req.body.PW;
+
+  const queries = [];
+
   if (file) {
     const query1 =
       "INSERT INTO userfiles (userID, ufName, ufPath, ufSize, ufRName) VALUES (?, ?, ?, ?, ?)";
@@ -89,6 +166,6 @@ function executeQuery(query, values) {
       }
     });
   });
-}
+}*/
 
 module.exports = router;
